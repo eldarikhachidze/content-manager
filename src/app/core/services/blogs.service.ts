4a3286f09beda3cf5c31a4d0a0bfb32947f19438
eldar <e.khachidze@alte.edu.ge>
2023-11-06 01:57:11 +0400

    feat: edit blogs

--- src/app/core/services/blogs.service.ts
+++ src/app/core/services/blogs.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {BaseService} from "./base.service";
 import {Observable} from "rxjs";
 import {Blog, PaginatedResponse} from "../interfaces/blogs";
@@ -7,24 +7,33 @@ import {User} from "../interfaces/user";
 @Injectable({
   providedIn: 'root'
 })
-export class BlogsService extends BaseService{
+export class BlogsService extends BaseService {
 
 
   // create(formData: FormData): Observable<Blog> {
   //   return this.post<Blog>('blog/create-blog', formData);
   // }
   create(title: string, description: string, file: File) {
-  const formData = new FormData();
-  formData.append('title', title);
-  formData.append('description', description);
-  formData.append('files', file);
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('description', description);
+    formData.append('files', file);
 
-  return this.post<any>('blog/create-blog', formData);
-}
+    return this.post<any>('blog/create-blog', formData);
+  }
 
   // create(blog: Blog): Observable<Blog> {
   //   return this.post<Blog>('blog/create-blog', blog);
   // }
+
+
+  update(id: string, blog: Blog): Observable<Blog> {
+    return this.put(`blog/update-blog/${id}`, blog)
+  }
+
+  getOne(id: string): Observable<Blog> {
+    return this.get(`blog/get-blog/${id}`)
+  }
 
   getAllBlogs(): Observable<PaginatedResponse<Blog[]>> {
     return this.get<PaginatedResponse<Blog[]>>('blog/get-all-blogs');
--- src/app/pages/blogs/blogs-add-or-edit/blogs-add-or-edit.component.ts
+++ src/app/pages/blogs/blogs-add-or-edit/blogs-add-or-edit.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {BlogsService} from "../../../core/services/blogs.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {of, pipe, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-blogs-add-or-edit',
   templateUrl: './blogs-add-or-edit.component.html',
   styleUrls: ['./blogs-add-or-edit.component.scss']
 })
-export class BlogsAddOrEditComponent {
+export class BlogsAddOrEditComponent implements OnInit {
   form: FormGroup = new FormGroup({
     id: new FormControl(null),
     title: new FormControl('', Validators.required),
@@ -16,31 +17,76 @@ export class BlogsAddOrEditComponent {
     files: new FormControl('', Validators.required),
   })
 
+  title: string = ''; // Initialize title
+  description: string = ''; // Initialize description
+  file!: File
+
   constructor(
     private blogsService: BlogsService,
     private router: Router,
+    private route: ActivatedRoute
   ) {
   }
 
 
-
-  submit() {
-    if (this.form.invalid) {
-      return
-    }
-
-    // console.log(this.form.value)
-    // const formData: FormData = new FormData();
-    //
-    // formData.append('id', this.form.get('id')?.value);
-    // formData.append('title', this.form.get('title')?.value);
-    // formData.append('description', this.form.get('description')?.value);
-    // formData.append('image', this.form.get('image')?.value);
-    //
-    // this.blogsService.create(formData).subscribe(res => {
-    //   this.router.navigate(['/blogs']).then(() => {
-    //     this.form.reset();
-    //   });
-    // });
+  ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap((params: any) => {
+        if (params['id']) {
+          return this.blogsService.getOne(params['id'])
+        }
+        return of(null)
+      })
+    ).subscribe(res => {
+      if (res) {
+        this.form.patchValue({
+          ...res
+        })
+      }
+    })
   }
-}
+
+
+
+    submit() {
+    //   if (this.form.invalid) {
+    //     return;
+    //   }
+    //
+    //   const formData: FormData = new FormData();
+    //   formData.append('id', this.form.get('id')?.value);
+    //   formData.append('title', this.form.get('title')?.value);
+    //   formData.append('description', this.form.get('description')?.value);
+    //   formData.append('files', this.form.get('files')?.value); // Assuming 'files' is the name of your file input field
+    //
+    //   this.blogsService.update(formData).subscribe(res => {
+    //     this.router.navigate(['/blogs']).then(() => {
+    //       this.form.reset();
+    //     });
+    //   });
+
+
+      if (this.form.invalid) {
+        return
+      }
+      if (this.form.value.id) {
+        this.blogsService.update(this.form.value.id, this.form.value)
+          .pipe()
+          .subscribe(res => {
+            this.router.navigate(['/blogs'])
+              .then(() => {
+                this.form.reset()
+              })
+          })
+      } else {
+        this.blogsService.create(this.title, this.description, this.file)
+          .pipe()
+          .subscribe(res => {
+            this.router.navigate(['/blogs'])
+              .then(() => {
+                this.form.reset()
+              })
+          })
+      }
+    }
+  }
